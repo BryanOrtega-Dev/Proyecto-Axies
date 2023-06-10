1e789a27f72605004009799357e259b7062349ad
BryanOrtega-Dev <bryanortega1994@gmail.com>
2023-06-09 23:39:46 -0600

    no habia subido estos blade por el gitignore

---
+++ resources/views/components/itemToday.blade.php
@@ -0,0 +1,27 @@
+<div class=" flex flex-col bg-secundary-grey rounded-Twenty p-5 w-[330px]">
+    <div class="w-[290px] h-[290px] bg-white-3 rounded-Twenty relative">
+        <div class="absolute top-[14px] left-3">
+            <x-comingSoon></x-comingSoon>
+        </div>
+        <div class="absolute top-[14px] right-3">
+            <x-viewLike></x-viewLike>
+        </div>
+        <div class="absolute top-[136px] left-[73px]">
+            <x-buttonPlace></x-buttonPlace>
+        </div>
+    </div>
+    <h4 class="mt-[21px] mb-[17px] text-white font-bold text-18px leading-[26px]">"Crypto Egg Stamp #5”</h4>
+    <div class="flex items-center justify-between mb-[19px]">
+        <x-creatorName></x-creatorName>
+        <x-bsc></x-bsc>
+    </div>
+    <div>
+        <div class="flex items-center justify-between pt-[15px] border-t border-like-color">
+            <div>
+                <h5 class="text-white-2 text-[13px] leading-5 font-normal">Current Bid</h5>
+                <h4 class="text-white text-18px leading-[26px] font-bold">4.89 ETH <span class="text-white-2 text-[13px] leading-5 font-normal">= $12.246</span></h4> 
+            </div>
+            <x-viewHistory></x-viewHistory>
+        </div>
+    </div>
+</div>
